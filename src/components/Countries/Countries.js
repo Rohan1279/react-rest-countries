feda26b46fd802ccd2b7c7f5d651ed170e396039
Rohan1279 <muniruddinrohan@gmail.com>
2022-09-23 09:17:03 +0600

    update2

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -12,6 +12,7 @@ const Countries = () => {
     <div>
       <h1>Hello from countries {countries.length}</h1>
       <h1>Updated from github</h1>
+      <h1>Second update from github</h1>
      <div  className="countries-container">
      {
       countries.map(country => <Country
